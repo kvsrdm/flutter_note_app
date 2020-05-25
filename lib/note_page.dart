f319c676da0876aaec524e37098bf0b79f59aa71
Kevser <kvser.erdm@gmail.com>
2020-05-25 23:09:30 +0300

    Add native ad

--- lib/note_page.dart
+++ lib/note_page.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_fader/flutter_fader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_native_admob/flutter_native_admob.dart';
+import 'package:flutter_native_admob/native_admob_controller.dart';
 import 'package:noteappfirebase/SpUtil.dart';
 import 'package:noteappfirebase/login_page.dart';
 import 'package:toast/toast.dart';
@@ -39,6 +43,15 @@ class NotePageState extends State<NotePage>
   List<String> _sortList = ['Alfabetik', 'Önce En Yeni', 'Önce En Eski'];
   String _selectedItem;
 
+  var bannerPadding = 0;
+
+  static const _adUnitID = "ca-app-pub-3940256099942544/2247696110";
+  // ca-app-pub-3940256099942544/2247696110
+  final _nativeAdController = NativeAdmobController();
+  double _height = 0;
+
+  StreamSubscription _subscription;
+
 /*  const _reviver(String key, value){
     if(key != null && value is Map && key.contains("-")) {
         return new NoteModel.fromJson(value);
@@ -52,6 +65,7 @@ class NotePageState extends State<NotePage>
 
   @override
   void initState() {
+    _subscription = _nativeAdController.stateChanged.listen(_onStateChanged);
     /* const jsonCodec = const JsonCodec(reviver: _reviver, toEncodable: _toEncodable);
     var noteFromCacheString = SpUtil.getString("NOTE_CACHE");
     noteFromCache = jsonCodec.decode(noteFromCacheString);*/
@@ -66,14 +80,31 @@ class NotePageState extends State<NotePage>
         });
       }
     }
-
     /* SpUtil.remove("NOTE_CACHE");*/
-
     list = List();
     getList();
     floatingActionButtonAnimation();
     super.initState();
     WidgetsBinding.instance.addObserver(this);
+  }
+
+  void _onStateChanged(AdLoadState state) {
+    switch (state) {
+      case AdLoadState.loading:
+        setState(() {
+          _height = 0;
+        });
+        break;
+
+      case AdLoadState.loadCompleted:
+        setState(() {
+          _height = 100;
+        });
+        break;
+
+      default:
+        break;
+    }
   }
 
   @override
@@ -113,157 +144,177 @@ class NotePageState extends State<NotePage>
       child: Scaffold(
         key: _scaffoldKey,
         resizeToAvoidBottomPadding: false,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            /*    RaisedButton(
-              onPressed: () {},
-              child: Text('Show SnackBar'),
-            ),*/
-            Container(
-              margin: EdgeInsets.only(right: 10, top: 10),
-              child: Align(
-                  alignment: Alignment.topRight,
-                  child: GestureDetector(
-                    onTap: () {
-                      if (_auth.currentUser() != null) {
-                        _auth.signOut().then((data) {
-                          /*Toast.show("Oturum kapatılıyor.", context,
-                              duration: Toast.LENGTH_LONG,
-                              gravity: Toast.BOTTOM);*/
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoginPage()));
-                          debugPrint("Çıkış yapıldı");
-                        }).catchError((onError) {
-                          debugPrint(
-                              "Çıkış yaparken bir hata oluştu. $onError");
-                        });
-                      } else {
-                        debugPrint("Kullanıcı yok.");
-                      }
-                    },
-                    child: Text(
-                      "Çıkış",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          fontSize: 18,
-                          color: Colors.deepPurple),
-                    ),
-                  )),
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                  child: Text(
-                    "Notlar",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  margin: EdgeInsets.only(right: 10, top: 10, bottom: 10),
-                  child: DropdownButton(
-                    hint: Text('Sıralama'), // Not necessary for Option 1
-                    value: _selectedItem,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _selectedItem = newValue;
-                        if (_selectedItem == _sortList[0]) {
-                          sortListString(list);
-                        } else if (_selectedItem == _sortList[1]) {
-                          sortListDateNew(list);
-                        } else if (_selectedItem == _sortList[2]) {
-                          sortListDateOld(list);
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              /*    RaisedButton(
+                onPressed: () {},
+                child: Text('Show SnackBar'),
+              ),*/
+              /*  SizedBox(
+                  height: bannerPadding
+                      .toDouble()), //TODO: Boyut farklı telefon boyutlarına göre düzenlenecek*/
+              SizedBox(height: 10),
+              Container(
+                margin: EdgeInsets.only(right: 10, top: 10),
+                child: Align(
+                    alignment: Alignment.topRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        if (_auth.currentUser() != null) {
+                          _auth.signOut().then((data) {
+                            /*Toast.show("Oturum kapatılıyor.", context,
+                                duration: Toast.LENGTH_LONG,
+                                gravity: Toast.BOTTOM);*/
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()));
+                            debugPrint("Çıkış yapıldı");
+                          }).catchError((onError) {
+                            debugPrint(
+                                "Çıkış yaparken bir hata oluştu. $onError");
+                          });
+                        } else {
+                          debugPrint("Kullanıcı yok.");
                         }
-                      });
-                    },
-                    items: _sortList.map((location) {
-                      return DropdownMenuItem(
-                        child: new Text(location),
-                        value: location,
-                      );
-                    }).toList(),
+                      },
+                      child: Text(
+                        "Çıkış",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 18,
+                            color: Colors.deepPurple),
+                      ),
+                    )),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                    child: Text(
+                      "Notlar",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Spacer(),
+                  Container(
+                    margin: EdgeInsets.only(right: 10, top: 10, bottom: 10),
+                    child: DropdownButton(
+                      hint: Text('Sıralama'), // Not necessary for Option 1
+                      value: _selectedItem,
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedItem = newValue;
+                          if (_selectedItem == _sortList[0]) {
+                            sortListString(list);
+                          } else if (_selectedItem == _sortList[1]) {
+                            sortListDateNew(list);
+                          } else if (_selectedItem == _sortList[2]) {
+                            sortListDateOld(list);
+                          }
+                        });
+                      },
+                      items: _sortList.map((location) {
+                        return DropdownMenuItem(
+                          child: new Text(location),
+                          value: location,
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  height: _height,
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  child: NativeAdmob(
+                    adUnitID: _adUnitID,
+                    controller: _nativeAdController,
+                    loading: Container(),
                   ),
                 ),
-              ],
-            ),
-            Expanded(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: list.isEmpty
-                    ? _emptyState()
-                    : ListView.builder(
-                        controller: _hideButtonController,
-                        itemBuilder: (BuildContext context, int index) {
-                          debugPrint("Liste bos değil");
-                          return Container(
-                            height: 120,
-                            child: GestureDetector(
-                              onTap: () {
-                                moveToNote(index);
-                              },
-                              child: Card(
-                                semanticContainer: true,
-                                clipBehavior: Clip.antiAliasWithSaveLayer,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                elevation: 5,
-                                margin: EdgeInsets.all(10),
-                                color: Color(0xFFf7f7f7),
-                                child: Row(
-                                  children: <Widget>[
-                                    SizedBox(width: 15),
-                                    Icon(Icons.note, color: Colors.deepPurple),
-                                    Spacer(),
-                                    Text(list[index].title),
-                                    Spacer(),
-                                    SizedBox(width: 15),
-                                    Text(list[index].date.split('-')[0]),
-                                    SizedBox(width: 15),
-                                    GestureDetector(
-                                      onTap: () {
-                                        _updateNote(!list[index].favorite,
-                                            list[index].id);
-                                        if (list[index].favorite == false) {
-                                          Toast.show(
-                                              "Not favorilerine eklendi.",
-                                              context,
-                                              duration: Toast.LENGTH_LONG,
-                                              gravity: Toast.BOTTOM);
-                                        } else {
-                                          Toast.show(
-                                              "Not favorilerinden kaldırıldı.",
-                                              context,
-                                              duration: Toast.LENGTH_LONG,
-                                              gravity: Toast.BOTTOM);
-                                        }
-                                      },
-                                      child: Container(
-                                          child: Icon(
-                                              list[index].favorite
-                                                  ? Icons.star
-                                                  : Icons.star_border,
-                                              color: Colors.amber)),
-                                    ),
-                                    SizedBox(width: 15),
-                                  ],
+              ),
+              Expanded(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: list.isEmpty
+                      ? _emptyState()
+                      : ListView.builder(
+                          controller: _hideButtonController,
+                          itemBuilder: (BuildContext context, int index) {
+                            debugPrint("Liste bos değil");
+                            return Container(
+                              height: 120,
+                              child: GestureDetector(
+                                onTap: () {
+                                  moveToNote(index);
+                                },
+                                child: Card(
+                                  semanticContainer: true,
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  elevation: 5,
+                                  margin: EdgeInsets.all(10),
+                                  color: Color(0xFFf7f7f7),
+                                  child: Row(
+                                    children: <Widget>[
+                                      SizedBox(width: 15),
+                                      Icon(Icons.note,
+                                          color: Colors.deepPurple),
+                                      Spacer(),
+                                      Text(list[index].title),
+                                      Spacer(),
+                                      SizedBox(width: 15),
+                                      Text(list[index].date.split('-')[0]),
+                                      SizedBox(width: 15),
+                                      GestureDetector(
+                                        onTap: () {
+                                          _updateNote(!list[index].favorite,
+                                              list[index].id);
+                                          if (list[index].favorite == false) {
+                                            Toast.show(
+                                                "Not favorilerine eklendi.",
+                                                context,
+                                                duration: Toast.LENGTH_LONG,
+                                                gravity: Toast.BOTTOM);
+                                          } else {
+                                            Toast.show(
+                                                "Not favorilerinden kaldırıldı.",
+                                                context,
+                                                duration: Toast.LENGTH_LONG,
+                                                gravity: Toast.BOTTOM);
+                                          }
+                                        },
+                                        child: Container(
+                                            child: Icon(
+                                                list[index].favorite
+                                                    ? Icons.star
+                                                    : Icons.star_border,
+                                                color: Colors.amber)),
+                                      ),
+                                      SizedBox(width: 15),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          );
-                        },
-                        itemCount: list == null ? 0 : list.length,
-                        // itemCount: list.length = 2,
-                      ),
+                            );
+                          },
+                          itemCount: list == null ? 0 : list.length,
+                          // itemCount: list.length = 2,
+                        ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
         floatingActionButton: Fader(
           controller: faderController,
@@ -329,6 +380,9 @@ class NotePageState extends State<NotePage>
   sortListDateOld(List<NoteModel> list) {
     //eskiden yeni tarihe göre sıralama işlemi
     list.sort((a, b) => a.date.toString().compareTo(b.date.toString()));
+
+    /* //TODO: Reklamları kaldır deneme aşaması
+    removeAds();*/
   }
 
   sortListString(List<NoteModel> list) {
